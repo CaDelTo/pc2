8b494139eb4ab8e4ab892d68803d68e60e1269d8
Cadeltoro <cadeltoro@uninorte.edu.co>
2025-05-16 16:27:35 -0500

    Refactor comments and clean up unused code in MPI communication

--- toro.py
+++ toro.py
@@ -3,23 +3,15 @@ from mpi4py import MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
-
-# Root process envía a cada proceso su rank
 if rank == 0:
     for i in range(size):
         comm.send(i, dest=i)
 else:
-    pass  # Los otros procesos no hacen nada aún
-
-# Todos los procesos (incluyendo root) reciben su número
+    pass 
 received_number = comm.recv(source=0)
-
-# Elevan al cuadrado
 squared = received_number ** 2
-
-# Envían el resultado al root
 if rank == 0:
-    results = [squared]  # Agrega el suyo
+    results = [squared]
     for i in range(1, size):
         result = comm.recv(source=i)
         results.append(result)
